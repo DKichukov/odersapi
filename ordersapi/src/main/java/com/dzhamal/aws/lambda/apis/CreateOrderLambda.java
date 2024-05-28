6d893786c9494601431e85a7b3e67defc1e648ef
DKichukov <dzhamal.kichukov@gmail.com>
2024-05-28 20:44:27 +0300

    add DynamoDB and put item into

--- ordersapi/src/main/java/com/dzhamal/aws/lambda/apis/CreateOrderLambda.java
+++ ordersapi/src/main/java/com/dzhamal/aws/lambda/apis/CreateOrderLambda.java
@@ -1,5 +1,9 @@
 package com.dzhamal.aws.lambda.apis;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.dzhamal.aws.lambda.apis.dto.Order;
@@ -12,6 +16,12 @@ public class CreateOrderLambda {
         throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         Order order = objectMapper.readValue(request.getBody(), Order.class);
+
+        DynamoDB dynamoDB = new DynamoDB(AmazonDynamoDBClientBuilder.defaultClient());
+        Table ordersTable = dynamoDB.getTable(System.getenv("ORDERS_TABLE"));
+        Item item = new Item().withPrimaryKey("id", order.getId()).withString("itemName", order.getItemName())
+            .withInt("quantity", order.getQuantity());
+        ordersTable.putItem(item);
 
         return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody("Order ID: " + order.getId());
     }
